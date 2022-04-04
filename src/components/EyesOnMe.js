febcf88f22c3a14e1c59905a909c826dbadc86c9
GarlicGrapes <austinlongocontracting@gmail.com>
2022-04-03 22:01:50 -0400

    commit tree 7bdc4c21b358b36e436eaf68c8c802ce15e962a3

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+function handleBlur() {
+    console.log("Hey! Eyes on me!")
+}
+
+function handleFocus() {
+    console.log("Good!")
+}
+
+function EyesOnMe(){
+    return (
+        <button onBlur={handleBlur} onFocus={handleFocus}>
+            Eyes on me
+        </button>
+    )
+}
+
+export default EyesOnMe;
